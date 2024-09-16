26e97e2067f019506c12bc85035898b8f63cb266
Delvin Villar Arroyo <delvinvillararroyo280398@gmail.com>
2024-09-15 18:26:58 -0600

    Agregar archivos de proyecto.

---
+++ propiedadTituloAurot/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace propiedadTituloAurot
+{
+
+    class libro
+    {
+     public string Titulo { get; set; }
+      public string Autor { get; set; }
+      public int Paginas  { get; set; }
+
+        public void MostrarIrformacion()
+        {
+            Console.WriteLine($"Titulo: {Titulo}");
+            Console.WriteLine($"Autor: {Autor}");
+            Console.WriteLine($"Paginas: {Paginas}");
+        }
+        public bool EsLargo()
+            {
+              return Paginas >500;
+            
+            }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            libro libro = new libro();
+            libro.Titulo = "Habito Atomico";
+            libro.Autor = " James Clear";
+            libro.Paginas = 600;
+            libro.MostrarIrformacion();
+
+            if (libro.EsLargo())
+            {
+                Console.WriteLine("Este libro es largo");
+            } else
+            {
+                Console.WriteLine("Este libro no es largo");
+            }
+        }
+    }
+}
